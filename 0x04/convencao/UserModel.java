6df7429a3ee68806361e2acde23d47d4f6972121
Diego <36318050+dcardoso25@users.noreply.github.com>
2025-10-04 14:27:56 -0300

    Create UserModel.java

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,26 @@
+// classe users, representando nossa entidade
+
+class UsersModel {
+
+    private int idUser;
+
+    private String descricao;
+
+    final static int IDADE_MINIMA = 18;
+
+
+
+    // Método que coleta todos os usuários
+    public List<UserModel> fetchUsers() {
+        return new ArrayList<>();
+    }
+
+    public List<UserModel> getAllUsers() {
+        return new ArrayList<>();
+    }
+
+    public List<UserModel> getUsers() {
+        return new ArrayList<>();
+    }
+
+}
